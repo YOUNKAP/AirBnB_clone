d5f675d57de79141a6d773677eeeaab06dbe1f21
YOUNKAP <nina.younkap@ccaa.pro>
2023-10-15 03:36:53 -0400

    pycodestyle compliance base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-Module for Base class
+"""Module for Base class
 defines all common attributes/methods for other classes.
 """
 
-#from models import storage
 from datetime import datetime
 import uuid
 
@@ -35,23 +33,19 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            #storage.new(self)
 
     def __str__(self):
-        """Returns returns printable string representation of  of an instance."""
-
+        """Returns returns printable string representation of an instance."""
         return "[{}] ({}) {}".\
             format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime."""
-
+        """updates the public instance attribute updated_at with the current
+         datetime."""
         self.updated_at = datetime.now()
-        #storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance."""
-
+        """returns a dictionary containing all keys/values of the instance."""
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat("T")
