feda0ff877685fb57413425b1cb435dcfdbd2f49
YOUNKAP <nina.younkap@ccaa.pro>
2023-10-15 15:44:03 -0400

    update console

--- console.py
+++ console.py
@@ -1,1 +1,75 @@
 #!/usr/bin/python3
+"""Entry point for command interpreter."""
+import cmd
+import re
+from shlex import split
+from models import storage
+from models.base_model import BaseModel
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the entry point for cmd interpreter.
+
+    Attributes:
+        prompt (str): The command prompt.
+    """
+
+    prompt = "(hbnb) "
+    __classes = {
+        "BaseModel"
+    }
+
+    def emptyline(self):
+        """Do nothing."""
+        pass
+
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
+    def do_quit(self, arg):
+        """Exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program."""
+        return True
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
