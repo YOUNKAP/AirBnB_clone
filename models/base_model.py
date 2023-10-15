ead1e65575b3e8326ad9105c83b5e0bbb4a73a50
YOUNKAP <nina.younkap@ccaa.pro>
2023-10-15 05:57:14 -0400

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -9,33 +9,36 @@ import uuid
 
 class BaseModel:
 
-    """Class for base model of objects.
+    """Class for base model of objects."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of  Base instance.
+
         Args:
             - id (str) : uuid  at instance creation
             - create_at(datetime) : datetime at instance creation
             - update_at (datetime): datetime at instance update
-    """
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+            - *args: list of argument
+            - **kwargs: dict of key-values argument
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-    @classmethod
-    def __str__(baseModel):
+    def __str__(self):
         """Returns returns printable string representation of an instance."""
         return "[{}] ({}) {}".\
-            format(type(baseModel).__name__, baseModel.id, baseModel.__dict__)
+            format(type(self).__name__, self.id, self.__dict__)
 
-    @classmethod
-    def save(baseModel):
+    def save(self):
         """updates the public instance attribute updated_at with the current
          datetime."""
-        baseModel.updated_at = datetime.now()
+        self.updated_at = datetime.now()
 
-    @classmethod
-    def to_dict(baseModel):
+    def to_dict(self):
         """returns a dictionary containing all keys/values of the instance."""
-        inst_dict = baseModel.__dict__.copy()
-        inst_dict["__class__"] = type(baseModel).__name__
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = type(self).__name__
         inst_dict["created_at"] = inst_dict["created_at"].isoformat("T")
         inst_dict["updated_at"] = inst_dict["updated_at"].isoformat("T")
         return inst_dict
